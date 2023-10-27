25b8185b855808aea8ff176bc9e0f9a20a51096e
abedghr <abed.ghandour7298@gmail.com>
2023-10-27 10:48:43 +0300

    preproduction commit

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new ValidationExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
